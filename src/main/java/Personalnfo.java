e71645c8c6be440ca2b5d5f78926c60d6de2244f
ChitdanaiSangnoi-6509681117 <144982052+ChitdanaiSangnoi-6509681117@users.noreply.github.com>
2023-11-20 23:42:39 +0700

    Branch Update: Personal Info Model
    model and html

---
+++ src/main/java/Personalnfo.java
@@ -0,0 +1,77 @@
+package cs.tu.studentSprint1.Model;
+
+public class Personalnfo {
+    private String tumbol;
+    private String amphur;
+
+    public String getTumbol() {
+        return tumbol;
+    }
+
+    public void setTumbol(String tumbol) {
+        this.tumbol = tumbol;
+    }
+
+    public String getAmphur() {
+        return amphur;
+    }
+
+    public void setAmphur(String amphur) {
+        this.amphur = amphur;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getCollegeYear() {
+        return collegeYear;
+    }
+
+    public void setCollegeYear(String collegeYear) {
+        this.collegeYear = collegeYear;
+    }
+
+    private String province;
+    private String postalCode;
+    private String mobileNo;
+    private String phoneNo;
+    private String teacher;
+    private String collegeYear;
+}
